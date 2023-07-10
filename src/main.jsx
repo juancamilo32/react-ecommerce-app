c05498b0e2a61d57c5f5f044908aa7d252f50d52
Juan Londoño <57278226+juancamilo32@users.noreply.github.com>
2023-07-10 16:04:25 -0500

    Page routing implemented

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { Home, Products, Product } from './routes';
+
+import { Navbar, Footer } from './components'
+
+const Layout = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home/Home";
+import Products from "./Products/Products";
+import Product from "./Product/Product";
+
+export {
+    Home,
+    Products,
+    Product
+}
