1d6980214a98e889a0c19917342a8df3a21925e3
Juan Londoño <57278226+juancamilo32@users.noreply.github.com>
2023-07-13 12:36:55 -0500

    Navbar implemented

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
 
+import { Link } from "react-router-dom";
+
+import './Navbar.css'
+
+import { logo } from '../../assets'
+import { BsSearch, BsPerson, BsCart } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseSharp } from "react-icons/io5";
+
 const Navbar = () => {
+
+    const [toggleMenu, setToggleMenu] = React.useState(false);
+
     return (
-        <div>Navbar</div>
+        <div className='app-navbar'>
+            <div className='app-navbar-logo'>
+                <Link to="/"><img src={logo} /></Link>
+            </div>
+            <div className='app-navbar-categories'>
+                <Link className="link" to="/products/1">Women</Link>
+                <Link className="link" to="/products/2">Men</Link>
+                <Link className="link" to="/products/3">Kids</Link>
+            </div>
+            <div className='app-navbar-buttons'>
+                <BsSearch size={20} className='icon icon-search' />
+                <BsPerson size={24} className='icon icon-profile' />
+                <div className='app-navbar-buttons-cart'>
+                    <BsCart size={20} className='icon' />
+                    <span className='app-navbar-buttos-cart-bubble'>1</span>
+                </div>
+                <div className='app-navbar-smallscreen'>
+                    <GiHamburgerMenu color='black' fontSize={20} onClick={() => setToggleMenu(true)} />
+                    {
+                        toggleMenu ?
+                            <div className='navbar-smallscreen-overlay slide-bottom'>
+                                <IoCloseSharp fontSize={25} className='navbar-smallscreen-overlay-close' onClick={() => setToggleMenu(false)} />
+                                <div className='app-navbar-smallscreen-items'>
+                                    <Link className="link" to="/products/1" onClick={() => setToggleMenu(false)}>Women</Link>
+                                    <Link className="link" to="/products/2" onClick={() => setToggleMenu(false)}>Men</Link>
+                                    <Link className="link" to="/products/3" onClick={() => setToggleMenu(false)}>Kids</Link>
+                                </div>
+                            </div> :
+                            null
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
